533c21718f0bf73341c4cd662459457f4ee4ea11
JPetta <jovipetraratulangi@gmail.com>
2020-02-07 14:37:47 +0700

    added stock details feature

--- client/js/jovi.js
+++ client/js/jovi.js
@@ -24,9 +24,9 @@ $(function(){
             result.forEach(stock => {
                 $('#most-active-table').append(`
                 <tr>
-                <td scope="row">${stock.companyName}</td>
-                <td>$${Math.floor(stock.price)}</td>
-                <td>${stock.changesPercentage}</td>
+                    <td class="stock-card" id="${stock.ticker}" scope="row">${stock.companyName}</td>
+                    <td>$${Math.floor(stock.price)}</td>
+                    <td>${stock.changesPercentage}</td>
                 </tr>
                 `)
             });
@@ -78,12 +78,52 @@ $(function(){
     })
 
     $(document).on('click', '.stock-card', function(){
-        $.ajax({
-            url : `http://localhost:3000/stocks/profile/${this.id}`,
-            type : 'get',
-            success : (result,status)=>{
-                console.log(result)
-            }
-        })
+        getStockDetail(this.id)
     })
 })
+
+function getStockDetail(id){
+    $.ajax({
+        url : `http://localhost:3000/stocks/profile/${id}`,
+        type : 'get',
+        success : (result,status)=>{
+            $('#stock-details').empty()
+            $('#news-section').hide()
+            $('#stock-details').append(`
+            <div class="col-12 border text-white" style="background-color: #343A40; color: white; ">
+                <h1 class="p-3">${result.profile.companyName}</h1>
+                <p class="pl-3 pr-3">${result.profile.website}</p>
+                <p class="pl-3 pr-3">CEO : ${result.profile.ceo}</p>
+                <p class="pl-3 pr-3">${result.profile.industry}</p>
+                <p class="pl-3 pr-3">${result.profile.description}</p>
+                <table class="table table-borderless table-dark">
+                    <thead>
+                    <tr>
+                        <th scope="col">Price</th>
+                        <th scope="col">Beta</th>
+                        <th scope="col">Average Vol</th>
+                        <th scope="col">Market Cap</th>
+                        <th scope="col">Range</th>
+                        <th scope="col">Changes</th>
+                        <th scope="col">Changes %</th>
+                        <th scope="col">Exchange</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>${result.profile.price}</td>
+                        <td>${result.profile.beta}</td>
+                        <td>${result.profile.volAvg}</td>
+                        <td>${result.profile.mktCap}</td>
+                        <td>${result.profile.range}</td>
+                        <td>${result.profile.changes}</td>
+                        <td>${result.profile.changesPercentage}</td>
+                        <td>${result.profile.exchange}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            `).hide().fadeIn(1000)
+        }
+    })
+}
